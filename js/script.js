ac18f4e74cf58f632e6d9ed459aaee781b20ec3f
Njoro410 <brian.njoroge@student.moringaschool.com>
2021-12-31 16:03:52 +0300

    added jquery toggle method for toggling between icons and description

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+  $(document).ready(function() {
+    $("#design").click(function() {
+      $(".wwd-description1").toggle();
+      $(".wwd-icons1").toggle();
+    });
+  });
+
+  
+  $(document).ready(function() {
+    $("#development").click(function() {
+      $(".wwd-icons2").toggle();
+      $(".wwd-description2").toggle();
+    });
+  });
+
+  $(document).ready(function() {
+    $("#product-management").click(function() {
+      $(".wwd-icons3").toggle();
+      $(".wwd-description3").toggle();
+    });
+  });
+
+  
