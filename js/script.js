f04f0fc4e2099961f858be8d8d29ce0f4283b7bf
Njoro410 <brian.njoroge@student.moringaschool.com>
2022-01-01 01:44:13 +0300

    added hover mouse event for portfolio section

--- js/script.js
+++ js/script.js
@@ -3,22 +3,43 @@
     $("#design").click(function() {
       $(".wwd-description1").toggle();
       $(".wwd-icons1").toggle();
-    });
-  });
-
-  
-  $(document).ready(function() {
+    })
     $("#development").click(function() {
       $(".wwd-icons2").toggle();
       $(".wwd-description2").toggle();
-    });
-  });
-
-  $(document).ready(function() {
+    })
     $("#product-management").click(function() {
       $(".wwd-icons3").toggle();
       $(".wwd-description3").toggle();
+    })
+    $("#work1").hover(function() {
+      $("#project-name1").toggle();
+    })
+    $("#work2").hover(function() {
+      $("#project-name2").toggle();
     });
+    $("#work3").hover(function() {
+      $("#project-name3").toggle();
+    });
+    $("#work4").hover(function() {
+      $("#project-name4").toggle();
+    });
+    $("#work5").hover(function() {
+      $("#project-name5").toggle();
+    });
+    $("#work6").hover(function() {
+      $("#project-name6").toggle();
+    });
+    $("#work7").hover(function() {
+      $("#project-name7").toggle();
+    });
+    $("#work8").hover(function() {
+      $("#project-name8").toggle();
+    });
+
+
+
+
   });
 
   
