087bd11581133cbc10a49eeef44553942147867d
Njoro410 <brian.njoroge@student.moringaschool.com>
2022-01-02 23:29:32 +0300

    added innerText attribute

--- js/script.js
+++ js/script.js
@@ -1,79 +1,54 @@
-  function submit() {
-    var jina = document.getElementById("name").value;
-    document.getElementById("outName").innerText = jina;
-      
-   } ;
-   
-   
-   
+function submit() {
+  var jina = document.getElementById("name").value;
+  document.getElementById("outName").innerText = jina;
+
+};
+
+//jQuery
+$(document).ready(function() {
+
+  $("#btn-sub").click(function() {
+    $("#feedbackModal").modal('show');
+  })
+  $(".btn-close").click(function() {
+    location.reload();
+  })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   $(document).ready(function() {
-
-    $("#btn-sub").click(function() {
-      $("#feedbackModal").modal('show');
-    })
-    $(".btn-close").click(function() {
-      location.reload();
-    })
-
-
-    $("#design").click(function() {
-      $(".wwd-description1").toggle();
-      $(".wwd-icons1").toggle();
-    })
-    $("#development").click(function() {
-      $(".wwd-icons2").toggle();
-      $(".wwd-description2").toggle();
-    })
-    $("#product-management").click(function() {
-      $(".wwd-icons3").toggle();
-      $(".wwd-description3").toggle();
-    })
-    $("#work1").hover(function() {
-      $("#project-name1").toggle();
-    })
-    $("#work2").hover(function() {
-      $("#project-name2").toggle();
-    });
-    $("#work3").hover(function() {
-      $("#project-name3").toggle();
-    })
-    $("#work4").hover(function() {
-      $("#project-name4").toggle();
-    })
-    $("#work5").hover(function() {
-      $("#project-name5").toggle();
-    })
-    $("#work6").hover(function() {
-      $("#project-name6").toggle();
-    })
-    $("#work7").hover(function() {
-      $("#project-name7").toggle();
-    })
-    $("#work8").hover(function() {
-      $("#project-name8").toggle();
-    })
+  $("#design").click(function() {
+    $(".wwd-description1").toggle();
+    $(".wwd-icons1").toggle();
+  })
+  $("#development").click(function() {
+    $(".wwd-icons2").toggle();
+    $(".wwd-description2").toggle();
+  })
+  $("#product-management").click(function() {
+    $(".wwd-icons3").toggle();
+    $(".wwd-description3").toggle();
+  })
+  $("#work1").hover(function() {
+    $("#project-name1").toggle();
+  })
+  $("#work2").hover(function() {
+    $("#project-name2").toggle();
   });
-
-  
+  $("#work3").hover(function() {
+    $("#project-name3").toggle();
+  })
+  $("#work4").hover(function() {
+    $("#project-name4").toggle();
+  })
+  $("#work5").hover(function() {
+    $("#project-name5").toggle();
+  })
+  $("#work6").hover(function() {
+    $("#project-name6").toggle();
+  })
+  $("#work7").hover(function() {
+    $("#project-name7").toggle();
+  })
+  $("#work8").hover(function() {
+    $("#project-name8").toggle();
+  })
+});
